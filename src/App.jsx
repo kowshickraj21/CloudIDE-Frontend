1bcd4b152620e9b4837dc08ad1c38ffc4f7c2196
kowshickraj21 <kowshickraj21.2005@gmail.com>
2024-10-26 11:12:09 +0530

    Landing Page

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import home from "../public/home1.png"
+function App() {
+  const [loaded, setLoaded] = useState(false);
+
+const handleLoad = () => {
+  setLoaded(true);
+}
+  window.addEventListener('load', handleLoad);
+
+  return (
+    <div className="h-svh bg-gradient-to-b from-[#1b3e48] to-black flex flex-row items-center font-sans">
+      <div className="font-bold text-white leading-snug">
+        <h1 className={`transition-all duration-300 ${loaded ? "ml-20 opacity-100 text-9xl":"-ml-20 opacity-0 text-8xl"}`}>Code.</h1>
+        <h1 className={`transition-all duration-500 ${loaded ? "ml-20 opacity-100 text-9xl":"-ml-20 opacity-0 text-8xl"}`}>Edit.</h1>
+        <h1 className={`transition-all duration-700 ${loaded ? "ml-20 opacity-100 text-9xl":"-ml-20 opacity-0 text-8xl"}`}>Deploy.</h1>
+        <div className="mt-20 ml-24 flex gap-5">
+        <button className="bg-green-600 text-white h-12 w-36 rounded-md shadow-md shadow-black">Try Now</button>
+        <button className="bg-opacity-10 bg-white text-green-600 border-green-600 border h-12 w-36 rounded-md shadow-md shadow-black">Learn More</button>
+        </div>
+      </div>
+      <div className={`absolute h-svh w-1/2 p-5 flex justify-center transition-all duration-300 ${loaded ? "opacity-100 right-10" : "opacity-0 -right-10"}`}>
+        <img src={home} alt="home" className="object-cover block"/>
+      </div>
+    </div>
+  )
+}
+
+export default App
