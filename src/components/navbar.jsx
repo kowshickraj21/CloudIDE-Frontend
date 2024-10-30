6a74ad3bd2dea9ab595401a40776c00092e256e0
kowshickraj21 <kowshickraj21.2005@gmail.com>
2024-10-30 17:19:33 +0530

    Added routing

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar = () => {
+  return (
+    <div className="h-14 w-full shadow-md flex justify-between items-center">
+      <p>Logo</p>
+      <div>
+      <button>Login</button>
+      <button>SignUp</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/console.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar"
+const Console = () => {
+  return (
+    <div>
+        <Navbar />
+      Console
+    </div>
+  )
+}
+
+export default Console
